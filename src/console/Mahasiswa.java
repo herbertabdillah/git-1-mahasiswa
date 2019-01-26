6ee0c1b42ed680a53dec20ae70c9ce4d58b419f3
herbertabdillah <abdillah.herbert@gmail.com>
2019-01-26 22:17:35 +0700

    IND : pisah menu mahasiswa

---
+++ src/console/Mahasiswa.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package console;
+
+import static console.Main.input;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Mahasiswa {
+    public static void menuMahasiswa(){
+        while(true){
+            System.out.println("no, nim, nama");
+            for(int i = 0; i < logic.Mahasiswa.arrayMahasiswa.size(); i++){
+                logic.Mahasiswa mahasiswa = logic.Mahasiswa.arrayMahasiswa.get(i);
+                System.out.println((i+1) + ", " + mahasiswa.getNim() + ", " + mahasiswa.getNama());
+            }
+            System.out.println();
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("3. Edit");
+            System.out.println("0. Kembali");
+            System.out.println("Pilihan : ");
+
+            String pilihan = input.nextLine();
+            if (pilihan.equals("1")) {
+                System.out.println("Belum Di Coding Broo!");
+            } else if (pilihan.equals("2")) {
+                System.out.println("Belum Di Coding Broo!");
+            } else if (pilihan.equals("3")) {
+                System.out.println("Belum Di Coding Broo!");
+            } else {
+               return;
+            }               
+        }
+
+    }
+}
